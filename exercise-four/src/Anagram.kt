1c30c1b3569c1676a9df878880d2b664a87272a8
Elliot Harvey <eharve04@dcs.bbk.ac.uk>
2018-01-10 13:24:55 +0000

    EX4

--- exercise-four/src/Anagram.kt
+++ exercise-four/src/Anagram.kt
@@ -1,6 +1,22 @@
 class Anagram(private val word: String) {
 
     fun match(anagrams: Collection<String>): Set<String> {
-        return setOf<String>()
+        var result = mutableSetOf<String>()
+        anagrams.forEach{x ->
+            var shouldBeDiscounted : Boolean = false
+            if (word.toLowerCase() == x.toLowerCase()) {shouldBeDiscounted = true}
+
+            var theChars  = word.toLowerCase().split("").map{it.trim()}.toMutableList()
+            var testedChars = x.toLowerCase().split("").map{it.trim()}.toMutableList()
+            var failedToRemove : Boolean = false
+            theChars.forEach{y ->
+                if (!testedChars.contains(y)) {failedToRemove = true}
+                testedChars.remove(y)
+            }
+            if (testedChars.isEmpty() && !failedToRemove &&
+                    !shouldBeDiscounted){result.add(x)}
+        }
+        result.forEach{x -> println(x)}
+        return result
     }
 }
