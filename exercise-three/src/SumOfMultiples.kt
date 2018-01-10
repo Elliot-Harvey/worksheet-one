7c2fe11c7ea3b1499c98f362672032b2712e9bd0
Elliot Harvey <eharve04@dcs.bbk.ac.uk>
2018-01-10 12:50:22 +0000

    EX3

--- exercise-three/src/SumOfMultiples.kt
+++ exercise-three/src/SumOfMultiples.kt
@@ -1,4 +1,21 @@
 object SumOfMultiples {
 
-    fun sum(factors: Set<Int>, limit: Int): Int = 0
+    fun sum(factors: Set<Int>, limit: Int): Int {
+        var Sum: Int = 0
+        var multiplesToAdd = mutableSetOf<Int>()
+        factors.forEach{x ->
+            var currentMultiple : Int = x
+            while (currentMultiple < limit){
+                multiplesToAdd.add(currentMultiple)
+                currentMultiple+=x
+            }
+        }
+
+        multiplesToAdd.forEach{x ->
+            Sum+=x
+        }
+
+        println(Sum)
+        return Sum;
+    }
 }
