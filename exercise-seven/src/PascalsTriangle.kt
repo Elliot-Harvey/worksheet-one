c3acaff4156a5b6455ffe98f30b1ec7cf14e503d
Elliot Harvey <eharve04@dcs.bbk.ac.uk>
2018-01-12 15:33:52 +0000

    EX7

--- exercise-seven/src/PascalsTriangle.kt
+++ exercise-seven/src/PascalsTriangle.kt
@@ -1,7 +1,28 @@
 object PascalsTriangle {
 
-    fun computeTriangle(rows: Int): List<List<Int>> {
-        // TODO
-        return listOf(listOf<Int>())
+    fun computeTriangle(rows: Int): List<List<Int>>{
+        if (rows < 0) {throw IllegalArgumentException("Rows can't be negative!")}
+
+        var result : MutableList<List<Int>> = mutableListOf<List<Int>>()
+        for (x in 1..rows){
+            // create the row, fill it with 1s, and add to structure
+            var thisRow : MutableList<Int> = mutableListOf<Int>()
+            for (y in 1..x){
+                thisRow.add(1)
+            }
+            // if it's not the first row, iterate through it and alter any values that are not the first or last
+            if (x > 1){
+                for (z in 1..thisRow.size-2){
+                    // find the row above and index to left and index to right
+                    var addition1 : Int = result.get(x-2).get(z-1)
+                    var addition2 : Int = result.get(x-2).get(z)
+                    thisRow.set(z,addition1+addition2)
+                }
+            }
+            result.add(thisRow)
+        }
+        println(result)
+
+        return result
     }
 }
--- exercise-six/src/PrimeFactorCalculator.kt
+++ exercise-six/src/PrimeFactorCalculator.kt
@@ -1,12 +1,52 @@
 object PrimeFactorCalculator {
 
     fun primeFactors(int: Int): List<Int> {
-        // TODO
-        return listOf<Int>()
+        var result = mutableListOf<Int>()
+        var factor : Int = 2
+        var currentVal : Int = int
+        while (currentVal != 1){
+            if (currentVal % factor != 0){
+                factor++
+                currentVal = int
+            }
+            else {
+                currentVal/=factor
+                if (checkIfPrime(factor)){
+                    result.add(factor)
+                }
+
+            }
+        }
+
+        return result
     }
 
     fun primeFactors(long: Long): List<Long> {
-        // TODO
-        return listOf<Long>()
+        var result = mutableListOf<Long>()
+        var factor : Long = 2
+        var currentVal : Long = long
+        while (currentVal != 1L){
+            if (currentVal % factor != 0L){
+                factor++
+                currentVal = long
+            }
+            else {
+                currentVal/=factor
+                if (checkIfPrime(factor.toInt())){
+                    result.add(factor)
+                }
+
+            }
+        }
+
+        return result
+    }
+
+    fun checkIfPrime(value: Int): Boolean{
+        for (x in 2..value-1){
+            if (value % x == 0) {return false}
+        }
+
+        return true;
     }
 }
